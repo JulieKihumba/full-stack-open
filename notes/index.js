4822ac28c95c9a72856e33c253328f4f0fbc7491
JulieKihumba <eng.juliekihumba@gmail.com>
2026-03-04 12:25:14 +0300

    `Added express.json middleware, API endpoints for fetching, deleting, and creating notes`

--- notes/index.js
+++ notes/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 const app = express();
 
+app.use(express.json());
+
 let notes = [
   {
     id: "1",
@@ -23,14 +25,58 @@ app.get("/", (request, response) => {
   response.send("<h1>HELLO WORLD!</h1>");
 });
 
+// fetching all the resources
 app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
 
+// fetching a single resource
 app.get("/api/notes/:id", (request, response) => {
   const id = request.params.id;
   const note = notes.find((note) => note.id === id);
-  response.json(note)
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+// deleting a resource
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+//generating id that will be + 1 of the the maxId
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+// adding/receiving new data
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    important: body.important || false,
+  };
+
+  notes = notes.concat(note);
+
+  console.log(note);
+
+  response.json(note);
 });
 
 const PORT = 3001;
