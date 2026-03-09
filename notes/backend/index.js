c64a58c3a58da4bd46f491c1b37e5be652bf2d1b
JulieKihumba <eng.juliekihumba@gmail.com>
2026-03-09 16:31:20 +0300

    `Added express.json middleware and API endpoints for notes`

--- notes/backend/index.js
+++ notes/backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let notes = [
   {
     id: "1",
@@ -19,8 +21,30 @@ let notes = [
   },
 ];
 
-app.use("/", (require, response) => {
+app.get("/", (require, response) => {
   response.send("server is running");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
