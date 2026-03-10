ed26ab51e005d2ea5c1ae0852ab0185482cc9673
JulieKihumba <eng.juliekihumba@gmail.com>
2026-03-10 16:27:59 +0300

    `Update persons.js service with new base URL`

--- phonebook/frontend/src/services/persons.js
+++ phonebook/frontend/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://phonebook-backend-taupe.vercel.app/";
+const baseUrl = "https://phonebook-backend-taupe.vercel.app/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
