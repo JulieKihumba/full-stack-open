7c309c0cd0b131422c6af1d51dcd7a94fc8dc87a
JulieKihumba <eng.juliekihumba@gmail.com>
2026-03-12 09:04:47 +0300

    `Updated backend code to use CORS, dotenv, and fixed API endpoints`

--- notes/backend/index.js
+++ notes/backend/index.js
@@ -1,76 +1,43 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const Note = require("./models/note");
 
+app.use(cors());
 app.use(express.json());
-
-const mongoose = require("mongoose");
-
-const password = process.argv[2];
-const url = `mongodb://kihumbajulie58_db_user:${password}@ac-zddxiyy-shard-00-00.l23cbod.mongodb.net:27017,ac-zddxiyy-shard-00-01.l23cbod.mongodb.net:27017,ac-zddxiyy-shard-00-02.l23cbod.mongodb.net:27017/noteApp?ssl=true&replicaSet=atlas-k926o3-shard-0&authSource=admin&retryWrites=true&w=majority`;
-
-mongoose.set("strictQuery", false);
-mongoose.connect(url, { family: 4 });
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model("Note", noteSchema);
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
-
-app.get("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
+});
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
   if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+    return response.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -80,7 +47,7 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
