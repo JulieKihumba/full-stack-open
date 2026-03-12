c6eee53a0e4fc88031f4b8b1d3ec7c0ac04aa524
JulieKihumba <eng.juliekihumba@gmail.com>
2026-03-12 09:26:11 +0300

    `Update PORT assignment in index.js to include default value 3001`

--- notes/backend/index.js
+++ notes/backend/index.js
@@ -50,7 +50,7 @@ app.delete("/api/notes/:id", (request, response) => {
   });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
