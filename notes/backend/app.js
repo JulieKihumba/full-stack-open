f218b2a4a6a0e159b61580c805c637d2083226f8
JulieKihumba <eng.juliekihumba@gmail.com>
2026-04-01 09:52:36 +0300

    `Update dependencies and add CORS configuration`

--- notes/backend/app.js
+++ notes/backend/app.js
@@ -1,34 +1,41 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-const notesRouter = require('./controllers/notes')
-const usersRouter=require('./controllers/users')
-const loginRouter = require('./controllers/login')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
+const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
-const app = express()
+const app = express();
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI, { family: 4 })
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error('error connection to MongoDB:', error.message)
-  })
+    logger.error("error connection to MongoDB:", error.message);
+  });
 
-app.use(express.static('dist'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(
+  cors({
+    origin: "https://notes-frontend-dusky.vercel.app/",
+  }),
+);
+app.use(express.static("dist"));
+app.use(express.json());
 
-app.use('/api/notes', notesRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
+app.use(middleware.requestLogger);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
-module.exports = app
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
